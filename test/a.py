3e1d080f4d00f47b2934d364f34a8637336acf2a
zmc_li <1912592745@qq.com>
2023-11-04 10:00:49 +0800

    增加了新的方法，修改了配置文件

--- test/a.py
+++ test/a.py
@@ -1,8 +1,13 @@
-from test.single import add, reset
-
-import requests
+from test.single import add
 
 
 class A:
     def __init__(self):
         self.a_class = add()
+
+
+def sub(a: int, b: int):
+    return a - b
+
+
+print(sub(5, 1))
